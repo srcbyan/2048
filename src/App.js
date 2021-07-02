dd264ec7792dae1c84192c24f3c1f1962a518261
An Tr <andrecee.tnn@gmail.com>
2021-07-02 19:40:58 +0700

    basic functions completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Block from "./Block";
 import { cloneDeep } from "lodash";
-import { useEvent, getColors } from "./util";
+import { useEvent } from "./util";
 
 function App() {
   const UP_ARROW = 38;
@@ -15,6 +15,8 @@ function App() {
     [0, 0, 0, 0],
     [0, 0, 0, 0]
   ]);
+
+  const [gameOver, setGameOver] = useState(false);
 
   const initialize = () => {
     let newGrid = cloneDeep(data);
@@ -40,6 +42,13 @@ function App() {
       if (newGrid[random1][random2] === 0) {
         newGrid[random1][random2] = Math.random() > 0.5 ? 2 : 4;
         added = true;
+      }
+      if (attempts > 50) {
+        gridFull = true;
+        let gameOver = checkGameOver();
+        if (gameOver) {
+          alert("Game Over!");
+        }
       }
     }
   };
@@ -225,8 +234,38 @@ function App() {
     }
   };
 
+  const checkGameOver = () => {
+    let checkLeft = swipeLeft(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkLeft)) {
+      return false;
+    }
+    let checkRight = swipeRight(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkRight)) {
+      return false;
+    }
+    let checckUp = swipeUp(true);
+    if (JSON.stringify(data) !== JSON.stringify(checckUp)) {
+      return false;
+    }
+    let checkDown = swipeDown(true);
+    if (JSON.stringify(data) !== JSON.stringify(checkDown)) {
+      return false;
+    }
+    return true;
+  };
 
-  
+  const resetGame = () => {
+    const newGrid = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+    addNumber(newGrid);
+    addNumber(newGrid);
+    setData(newGrid);
+  };
+
   const handleKey = (event) => {
     switch (event.keyCode) {
       case UP_ARROW:
@@ -244,6 +283,13 @@ function App() {
       default:
         break;
     }
+
+    let gameOver = checkGameOver();
+    if (gameOver) {
+      alert("GAME OVER!");
+      setGameOver(true);
+      return;
+    }
   };
 
   useEffect(() => {
@@ -253,16 +299,42 @@ function App() {
   useEvent("keydown", handleKey);
 
   return (
-    <div className="bg-red-900 max-w-max m-auto p-2 rounded mt-5 bg-opacity-60">
-      {data.map((row, i) => {
-        return (
-          <div className="flex">
-            {row.map((digit, index) => (
-              <Block num={digit} key={index} />
-            ))}
+    <div className="flex items-start mt-10 m-auto justify-evenly">
+      <div className="bg-pink-500 flex flex-col flex-1 justify-end px-5">
+        <div>
+          <h1>2048</h1>
+        </div>
+        <div>
+          <h3>Join the tiles, get to 2048!</h3>
+        </div>
+        <div>
+          <h2>How to play:</h2>
+        </div>
+        <div>
+          <p>
+            Use your arrow keys to move the tiles. Tiles with the same number
+            merge into one when they touch. Add them up to reach 2048!
+          </p>
+        </div>
+      </div>
+      <div className="bg-red-900 max-w-max p-2 rounded  bg-opacity-60">
+        {data.map((row, i) => {
+          return (
+            <div className="flex">
+              {row.map((digit, index) => (
+                <Block num={digit} key={index} />
+              ))}
+            </div>
+          );
+        })}{" "}
+      </div>
+
+      <div className='bg-pink-300 flex-1 px-5'>
+          instruction bar
+          <h1>Score: </h1>
+          <h1>Best: </h1>
+          <button onClick={resetGame} className='text-white bg-gray-700'>New Game</button>
           </div>
-        );
-      })}
     </div>
   );
 }
